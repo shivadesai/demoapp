dab8def99fb085779002173c5b359f9acfdbd805
EE207599 <EE207599@lpg-ee207599.sasken.com>
2014-07-15 12:53:40 +0530

    removed comments

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Just a Title</title>
+    </head>
+    <body>
+        <?php
+       echo "Just for Testing ";
+        ?>
+    </body>
+</html>
